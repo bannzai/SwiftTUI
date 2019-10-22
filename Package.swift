bfb82a43ff9d5bb7e5b2fc8b7a696999971bd23e
bannzai <kingkong999yhirose@gmail.com>
2019-10-22 22:46:16 +0900

    :memo: Fix Package.swift

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     products: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .executable(name: "Demo", targets: ["SwiftTUI"]),
+        .executable(name: "Demo", targets: ["Demo"]),
         .library(
           name: "SwiftTUI",
           targets: ["SwiftTUI"]),
@@ -17,6 +17,9 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "Demo",
+            dependencies: ["SwiftTUI"]),
         .target(
             name: "SwiftTUI",
             dependencies: []),
