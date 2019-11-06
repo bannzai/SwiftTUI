2c865de1e36a8e2a6b447b8f2f0f53dc516738e8
bannzai <kingkong999yhirose@gmail.com>
2019-11-06 12:38:28 +0900

    :sparkles: :construction: Implement Text
    #
    # ==== Emojis ====
    # 🐛  :bug: バグ修正
    # 🤘  :metal: 機能改善
    # 🐚  :shell: コマンドを実行した結果とか
    # ✨  :sparkles: 部分的な機能追加
    # 🎉  :tada: 盛大に祝うべき大きな機能追加
    # 💉  :syringe: libraryの追加
    # 📝  :memo: ドキュメント・READMEの整備
    # ♻️   :recycle: リファクタリング
    # 🚿  :shower: 不要な機能・使われなくなった機能の削除
    # 💚  :green_heart: テストやCIの修正・改善
    # 👕  :shirt: Lintエラーの修正やコードスタイルの修正
    # 🚀  :rocket: パフォーマンス改善
    # 🆙  :up: 依存パッケージなどのアップデート
    # 🔒  :lock: 新機能の公開範囲の制限
    # 👮  :cop: セキュリティ関連の改善
    # 🎨  :art: デザイン修正
    # 🚧  :construction: WIP 一時的なコミット
    
    # ==== Format ====
    # :emoji: Subject
    #
    # Commit body...
    
    # ==== The Seven Rules ====
    # 1. Separate subject from body with a blank line
    # 2. Limit the subject line to 50 characters
    # 3. Capitalize the subject line
    # 4. Do not end the subject line with a period
    # 5. Use the imperative mood in the subject line
    # 6. Wrap the body at 72 characters
    # 7. Use the body to explain what and why vs. how
    #
    # How to Write a Git Commit Message http://chris.beams.io/posts/git-commit/
    
    # reference from http://memo.goodpatch.co/2016/07/beautiful-commits-with-emojis/
    # for https://github.com/bannzai/commit_template

---
+++ Sources/SwiftTUI/View/Text.swift
@@ -0,0 +1,140 @@
+//
+//  Text.swift
+//  Demo
+//
+//  Created by Yudai.Hirose on 2019/11/06.
+//
+
+import Foundation
+
+/// A view that displays one or more lines of read-only text.
+public struct Text {
+    let content: String
+    /// Creates an instance that displays `content` verbatim.
+    @inlinable public init(verbatim content: String) {
+        self.init(content)
+    }
+    
+    /// Creates an instance that displays `content` verbatim.
+    public init<S>(_ content: S) where S : StringProtocol {
+        self.content = String(content)
+    }
+
+}
+
+extension Text: Equatable {
+    public static func == (lhs: Text, rhs: Text) -> Bool {
+        return lhs.content == rhs.content
+    }
+}
+
+extension Text {
+    public static func + (lhs: Text, rhs: Text) -> Text {
+        return Text(lhs.content + rhs.content)
+    }
+}
+
+extension Text: View {
+    public typealias Body = Never
+}
+
+
+// TODO: Implement
+// Mark: - Text interfaces
+extension Text {
+    /// Sets the color of this text.
+    ///
+    /// - Parameter color: The color to use when displaying this text.
+    /// - Returns: Text that uses the color value you supply.
+    public func foregroundColor(_ color: Color?) -> Text {
+        return self
+    }
+
+    /// Sets the font to use when displaying this text.
+    ///
+    /// - Parameter font: The font to use when displaying this text.
+    /// - Returns: Text that uses the font you specify.
+    public func font(_ font: Font?) -> Text {
+        return self
+    }
+
+    /// Sets the font weight of this text.
+    ///
+    /// - Parameter weight: One of the available font weights.
+    /// - Returns: Text that uses the font weight you specify.
+    public func fontWeight(_ weight: Font.Weight?) -> Text {
+        return self
+    }
+
+    /// Applies a bold font weight to this text.
+    ///
+    /// - Returns: Bold text.
+    public func bold() -> Text {
+        return self
+    }
+
+    /// Applies italics to this text.
+    ///
+    /// - Returns: Italic text.
+    public func italic() -> Text {
+        return self
+    }
+
+    /// Applies a strikethrough to this text.
+    ///
+    /// - Parameters:
+    ///   - active: A Boolean value that indicates whether the text has a
+    ///     strikethrough applied.
+    ///   - color: The color of the strikethrough. If `color` is `nil`, the
+    ///     strikethrough uses the default foreground color.
+    /// - Returns: Text with a line through its center.
+    public func strikethrough(_ active: Bool = true, color: Color? = nil) -> Text {
+        return self
+    }
+
+    /// Applies an underline to this text.
+    ///
+    /// - Parameters:
+    ///   - active: A Boolean value that indicates whether the text has an
+    ///     underline.
+    ///   - color: The color of the underline. If `color` is `nil`, the
+    ///     underline uses the default foreground color.
+    /// - Returns: Text with a line running along its baseline.
+    public func underline(_ active: Bool = true, color: Color? = nil) -> Text {
+        return self
+    }
+
+    /// Sets the kerning for this text.
+    ///
+    /// - Parameter kerning: How many points the following character should be
+    ///   shifted from its default offset as defined by the current character's
+    ///   font in points; a positive kerning indicates a shift farther along
+    ///   and a negative kern indicates a shift closer to the current character.
+    /// - Returns: Text with the specified amount of kerning.
+    public func kerning(_ kerning: CGFloat) -> Text {
+        return self
+    }
+
+    /// Sets the tracking for this text.
+    ///
+    /// - Parameter tracking: The tracking attribute indicates how much
+    ///   additional space, in points, should be added to each character cluster
+    ///   after layout. The effect of this attribute is similar to `kerning()`
+    ///   but differs in that the added tracking is treated as trailing
+    ///   whitespace and a non-zero amount disables non-essential ligatures.
+    /// - Returns: Text with the specified amount of tracking.
+    ///   If both `kerning()` and `tracking()` are present, `kerning()` will be
+    ///   ignored; `tracking()` will still be honored.
+    public func tracking(_ tracking: CGFloat) -> Text {
+        return self
+    }
+
+    /// Sets the baseline offset for this text.
+    ///
+    /// - Parameter baselineOffset: The amount to shift the text vertically
+    ///   (up or down) in relation to its baseline.
+    /// - Returns: Text that's above or below its baseline.
+    public func baselineOffset(_ baselineOffset: CGFloat) -> Text {
+        return self
+    }
+}
