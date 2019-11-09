de66da1869a150d64f64ae05653655c481b768ff
bannzai <kingkong999yhirose@gmail.com>
2019-11-09 09:44:04 +0900

    :recycle: Add group of Visitor

---
+++ Sources/SwiftTUI/Visitor/Visitor.swift
@@ -0,0 +1,14 @@
+//
+//  Visitor.swift
+//  SwiftTUI
+//
+//  Created by Yudai.Hirose on 2019/11/08.
+//
+
+import Foundation
+
+// TODO: to internal type
+public protocol Visitor {
+    associatedtype VisitorResult
+    func visit<T>(_ content: T) -> VisitorResult
+}
