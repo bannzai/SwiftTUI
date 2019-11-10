1bca0b3d89ec362baf611fca61435993b3fe2aea
bannzai <kingkong999yhirose@gmail.com>
2019-11-10 23:00:42 +0900

    :sparkels: Add view visitor

--- Sources/SwiftTUI/Visitor/ViewVisitor.swift
+++ Sources/SwiftTUI/Visitor/ViewVisitor.swift
@@ -8,10 +8,29 @@
 import Foundation
 
 // TOOD: Internal
-//public struct ViewVisitor: Visitor {
-//    public typealias VisitorResult = SwiftTUIContentType
-//
-//    public func visit<T>(_ content: T) -> SwiftTUIContentType {
-//        return content.content()
-//    }
-//}
+open class TypeErasureVisitor: Visitor {
+    public typealias VisitoResult = SwiftTUIContentType
+    
+    open func visit<T>(_ content: T) -> VisitoResult {
+        fatalError("Should override this method to subclass")
+    }
+}
+
+public class ViewVisitor: TypeErasureVisitor {
+    public override func visit<T: View & Contentable>(_ content: T) -> VisitoResult {
+        return content.content()
+    }
+}
+
+public protocol TupleViewVisitor {
+    associatedtype VisitResult
+
+    func visit<T>(_ view: TupleView<T>) -> VisitResult
+}
+
+open class TypeErasureTupleViewVisitor: TupleViewVisitor {
+    public typealias VisitResult = SwiftTUIContentType
+    public func visit<T>(_ view: TupleView<T>) -> VisitResult {
+        fatalError("Should override this method to subclass")
+    }
+}
