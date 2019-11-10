47c4922354f72cae8ca3fe98820ef133034404c2
bannzai <kingkong999yhirose@gmail.com>
2019-11-10 23:02:46 +0900

    :sparkels: add typealias of SwiftTUIContentType

---
+++ Sources/SwiftTUI/Core/SwiftTUIConentType.swift
@@ -0,0 +1,11 @@
+//
+//  SwiftTUIConentType.swift
+//  SwiftTUI
+//
+//  Created by Yudai.Hirose on 2019/11/10.
+//
+
+import Foundation
+
+// TODO: Internal
+public typealias SwiftTUIContentType = String
