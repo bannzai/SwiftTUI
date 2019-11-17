46ffdae4a162cd2cccd81e3b72a54be19b3fce72
bannzai <kingkong999yhirose@gmail.com>
2019-11-17 22:01:18 +0900

    :recycle: Reanme directories

---
+++ Terminal/Terminal.swift
@@ -0,0 +1,29 @@
+//
+//  Terminal.swift
+//  SwiftTUI
+//
+//  Created by Yudai.Hirose on 2019/11/17.
+//
+
+import Foundation
+internal struct Terminal {
+    static internal func colorize(color: Color, content: SwiftTUIContentType) -> SwiftTUIContentType {
+
+    }
+}
+
+internal struct TPut {
+    internal let color: Color
+    internal let content: String
+
+    static let foregroundEscapeScequence = ""
+    
+    
+    func decompose() -> String {
+
+    }
+    
+    func debug() {
+        
+    }
+}
