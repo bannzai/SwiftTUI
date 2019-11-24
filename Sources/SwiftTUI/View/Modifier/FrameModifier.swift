78e28a47494e8c794d97dea3861598a28b32d530
bannzai <kingkong999yhirose@gmail.com>
2019-11-25 01:35:06 +0900

    :recycle: Fix frame layout

--- Sources/SwiftTUI/View/Modifier/FrameModifier.swift
+++ Sources/SwiftTUI/View/Modifier/FrameModifier.swift
@@ -7,33 +7,16 @@
 
 import Foundation
 
-@frozen public struct _FrameLayout: ViewModifier {
-    internal let width: CoreGraphics.CGFloat?
-    internal let height: CoreGraphics.CGFloat?
-    internal let alignment: Alignment
-    
-    @usableFromInline
-    internal init(width: CoreGraphics.CGFloat?, height: CoreGraphics.CGFloat?, alignment: Alignment) {
-        self.width = width
-        self.height = height
-        self.alignment = alignment
-    }
-    public typealias Body = Swift.Never
-    
-    // TODO:
-//    public typealias AnimatableData = EmptyAnimatableData
-}
-
-
 @available(OSX 10.15.0, *)
 extension View {
-    @inlinable public func frame(width: CoreGraphics.CGFloat? = nil, height: CoreGraphics.CGFloat? = nil, alignment: Alignment = .center) -> some View {
-        return modifier(
-            _FrameLayout(width: width, height: height, alignment: alignment))
+    public func frame(width: PhysicalDistance? = nil, height: PhysicalDistance? = nil) -> some View {
+        self._baseProperty?.width = width
+        self._baseProperty?.height = height
+        return self
     }
     
-    @inlinable public func frame() -> some View {
-        return frame(width: nil, height: nil, alignment: .center)
+    public func frame() -> some View {
+        return frame(width: nil, height: nil)
     }
 }
 
