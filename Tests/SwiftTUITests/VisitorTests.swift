d132ba29adbb4fc0cdf70a14c9d52a7b6ba8fb1f
bannzai <kingkong999yhirose@gmail.com>
2019-11-27 01:36:54 +0900

    :recycle: Fix view visitor

--- Tests/SwiftTUITests/VisitorTests.swift
+++ Tests/SwiftTUITests/VisitorTests.swift
@@ -12,21 +12,6 @@ import Runtime
 
 @available(OSX 10.15.0, *)
 class VisitorTests: XCTestCase {
-    class TestVisitor: AnyViewVisitor {
-        override func visit<T: View>(_ content: T) -> SwiftTUIContentType {
-            if let content = content as? Text {
-                return visit(content)
-            }
-            if let content = content as? EmptyView {
-                return visit(content)
-            }
-            return visit(content.body)
-        }
-        func visit(_ content: Text) -> SwiftTUIContentType { content.accept(visitor: self) }
-        func visit(_ content: EmptyView) -> SwiftTUIContentType { content.accept(visitor: self) }
-        func visit<T: View>(_ content: Group<T>) -> SwiftTUIContentType { content.accept(visitor: self) }
-    }
-    
     struct CustomView: View {
         var body: some View {
             EmptyView()
@@ -36,13 +21,13 @@ class VisitorTests: XCTestCase {
     func testViewVisitor() {
         XCTContext.runActivity(named: "when CustomView") { (_) in
             let view = CustomView()
-            let visitor = TestVisitor()
+            let visitor = ViewVisitor()
             let result = visitor.visit(view)
             XCTAssertEqual(result, "")
         }
         XCTContext.runActivity(named: "when Text with content") { (_) in
             let view = Text("hoge")
-            let visitor = TestVisitor()
+            let visitor = ViewVisitor()
             let result = visitor.visit(view)
             XCTAssertEqual("hoge", result)
         }
