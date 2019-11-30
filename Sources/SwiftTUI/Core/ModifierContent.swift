56496b7ee56ed954030fae0b1a1e4a742aa6ccc4
bannzai <kingkong999yhirose@gmail.com>
2019-11-30 16:34:22 +0900

    :sparkles: :construction: fix modifier example

--- Sources/SwiftTUI/Core/ModifierContent.swift
+++ Sources/SwiftTUI/Core/ModifierContent.swift
@@ -7,7 +7,8 @@
 
 import Foundation
 
-@frozen public struct ModifiedContent<Content, Modifier> {
+@available(OSX 10.15.0, *)
+@frozen public struct ModifiedContent<Content, Modifier> where Content: View, Modifier: ViewModifier {
   public var content: Content
   public var modifier: Modifier
   @inlinable public init(content: Content, modifier: Modifier) {
@@ -22,19 +23,25 @@ extension ModifiedContent : Swift.Equatable where Content : Swift.Equatable, Mod
     }
 }
 
-extension ModifiedContent: ViewAcceptable where Content: View, Modifier: ViewModifier {
+extension ModifiedContent: ViewAcceptable {
     public func accept<V>(visitor: V) -> V.VisitResult where V: Visitor {
         fatalError("TODO: Implement \(#function). But now, it can not call. because Body is never")
     }
 }
 
-extension ModifiedContent: View where Content: View, Modifier: ViewModifier {
+internal extension ModifiedContent {
+    mutating func modifed<Value>(keyPath: WritableKeyPath<Content, Value>, value: Value) {
+        content[keyPath: keyPath] = value
+    }
+}
+
+extension ModifiedContent: View {
     public typealias Body = Swift.Never
     public var _baseProperty: _ViewBaseProperties? { nil }
 }
 
 extension View {
-    @inlinable public func modifier<T>(_ modifier: T) -> ModifiedContent<Self, T> {
+    @inlinable public func modifier<T: ViewModifier>(_ modifier: T) -> ModifiedContent<Self, T> {
         return .init(content: self, modifier: modifier)
     }
 }
