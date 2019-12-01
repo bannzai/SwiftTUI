a2e66dbc4a6598e2dd833df72937a8102f3d6164
bannzai <kingkong999yhirose@gmail.com>
2019-12-02 02:56:20 +0900

    :sparekls: :construction: Add debug counter

--- Sources/SwiftTUI/Logger/Debug.swift
+++ Sources/SwiftTUI/Logger/Debug.swift
@@ -7,15 +7,22 @@
 
 import Foundation
 
+// NOTE: prevent infite loop
+fileprivate var limit = 100
+fileprivate var calledCount = 0
 struct DebugLogger {
     static let prefix = "DEBUG: "
-    
+
     func debug(
         function: String = #function,
         file: String = #file,
         line: Int = #line,
         userInfo: CustomDebugStringConvertible? = nil
         ) {
+        calledCount += 1
+        if limit < calledCount {
+            fatalError("Limited logger count")
+        }
         switch userInfo {
         case nil:
             print("\(DebugLogger.prefix) function: \(function), file: \(file), line: \(line)")
