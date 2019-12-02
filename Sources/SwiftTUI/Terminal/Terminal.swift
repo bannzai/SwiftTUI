f52a8ba4955913d320550519126b96c0c3236f17
bannzai <kingkong999yhirose@gmail.com>
2019-12-03 02:17:06 +0900

    :sparkles: add terminal file
    #
    # ==== Emojis ====
    # 🐛  :bug: バグ修正
    # 🤘  :metal: 機能改善
    # 🐚  :shell: コマンドを実行した結果とか
    # ✨  :sparkles: 部分的な機能追加
    # 🎉  :tada: 盛大に祝うべき大きな機能追加
    # 💉  :syringe: libraryの追加
    # 📝  :memo: ドキュメント・READMEの整備
    # ♻️   :recycle: リファクタリング
    # 🚿  :shower: 不要な機能・使われなくなった機能の削除
    # 💚  :green_heart: テストやCIの修正・改善
    # 👕  :shirt: Lintエラーの修正やコードスタイルの修正
    # 🚀  :rocket: パフォーマンス改善
    # 🆙  :up: 依存パッケージなどのアップデート
    # 🔒  :lock: 新機能の公開範囲の制限
    # 👮  :cop: セキュリティ関連の改善
    # 🎨  :art: デザイン修正
    # 🚧  :construction: WIP 一時的なコミット
    
    # ==== Format ====
    # :emoji: Subject
    #
    # Commit body...
    
    # ==== The Seven Rules ====
    # 1. Separate subject from body with a blank line
    # 2. Limit the subject line to 50 characters
    # 3. Capitalize the subject line
    # 4. Do not end the subject line with a period
    # 5. Use the imperative mood in the subject line
    # 6. Wrap the body at 72 characters
    # 7. Use the body to explain what and why vs. how
    #
    # How to Write a Git Commit Message http://chris.beams.io/posts/git-commit/
    
    # reference from http://memo.goodpatch.co/2016/07/beautiful-commits-with-emojis/
    # for https://github.com/bannzai/commit_template

--- Sources/SwiftTUI/Terminal/Terminal.swift
+++ Sources/SwiftTUI/Terminal/Terminal.swift
@@ -6,21 +6,38 @@
 //
 
 import Foundation
+import Darwin
 
-internal struct Terminal {
+internal struct Terminal { }
     
-    // MARK: - Decorate Color
-    // See: https://misc.flogisoft.com/bash/tip_colors_and_formatting
+// MARK: - Decorate Color
+extension Terminal {
     struct ColorDecorate {
         static internal let escapeSequence = "\u{001B}" // NOTE: \e
         static internal let start = "\(escapeSequence)["
         static internal let end = "\(escapeSequence)[0m"
     }
+    
+    // See: https://misc.flogisoft.com/bash/tip_colors_and_formatting
     static internal func _colorize(color: Color.Value, content: SwiftTUIContentType) -> SwiftTUIContentType {
         return ColorDecorate.start + "\(color)" + "m" + content + ColorDecorate.end
     }
     static dynamic internal func colorize(color: Color.Value, content: SwiftTUIContentType) -> SwiftTUIContentType {
         _colorize(color: color, content: content)
     }
-    
+}
+
+// MARK: - IO
+extension Terminal {
+    struct File {
+        // FIXME: Run on Xcode
+        static let input: FileHandle = {
+            let fileDescriptor = Darwin.open("/dev/tty", Darwin.O_RDONLY)
+            return FileHandle(fileDescriptor: fileDescriptor)
+        }()
+        static let output: FileHandle = {
+            let fileDescriptor = Darwin.open("/dev/tty", Darwin.O_WRONLY)
+            return FileHandle(fileDescriptor: fileDescriptor)
+        }()
+    }
 }
