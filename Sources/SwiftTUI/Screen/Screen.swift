44a170735c6963785ef4721ed25619330b92df1a
bannzai <kingkong999yhirose@gmail.com>
2019-12-07 19:10:00 +0900

    :sparkles: add window class

--- Sources/SwiftTUI/Screen/Screen.swift
+++ Sources/SwiftTUI/Screen/Screen.swift
@@ -8,67 +8,33 @@
 import Foundation
 import cncurses
 
-public struct Screen {
+public class Screen {
     public static let shared = Screen()
-    private init() {
-        screen = initscr()
-    }
+    private init() { }
     
-    // NOTE: ncurses root object name is `SCREEN`.
-    // But defined type name is `WINDOW`.
-    internal typealias _Screen = UnsafeMutablePointer<cncurses.WINDOW>
-    // NOTE: Keep screen
-    internal var screen: _Screen!
-    
-    // NOTE: access standard `Screen`. Maybe this is root screen.
-    internal var standardScreen: _Screen { stdscr }
+    internal var windows: [Window] = []
+
+    // NOTE: access stdscr. Maybe this is root screen.
+    internal var keyWindow: Window { windows.first(where: { $0.window == stdscr })! }
 }
 
-
-// MARK - Named ncurses functions
 private extension Screen {
-    func enableNoBufferingMode() {
-        // NOTE: Configure about immediately receive input keyevent
-        cbreak()
-    }
-    func setupNoEchoOnWindow() {
-        noecho()
-    }
-    func enableCursor() {
-        let enable: Int32 = 1
-        curs_set(enable)
-    }
-    func callUseColor() {
-        start_color()
-    }
-    func configureColorPairs() {
-        // NOTE: Range of 1 ~ (COLOR_PAIRS-1). If set 0, Setting foreground color is white and background color is black.
-        // And It can not changed color pair.
-        let pairNumber: Int16 = 1
-        init_pair(pairNumber, Int16(COLOR_BLACK), Int16(COLOR_WHITE))
-    }
-    func setupKeypad() {
-        // NOTE: If you set false, return character with escape sequence when input with function key
-        let returnRawKeyCode = true
-        keypad(standardScreen, returnRawKeyCode)
+    func append(window: Window) {
+        windows.append(window)
     }
 }
 
-// MARK: - Internal Named ncurses functions
 internal extension Screen {
     func setup() {
-        enableNoBufferingMode()
-        setupNoEchoOnWindow()
-        callUseColor()
-        enableCursor()
-        configureColorPairs()
-        setupKeypad()
-        clear();
+        if !windows.isEmpty {
+            assertionFailure("duplicated call setup functions")
+        }
+        let window = Window()
+        window.setup()
+        append(window: window)
     }
     func dispose() {
-        if !isendwin() {
-            endwin()
-        }
-        delscreen(OpaquePointer(screen))
+        keyWindow.dispose()
+        windows.remove(at: windows.firstIndex (where: { $0 === keyWindow })!)
     }
 }
--- Sources/SwiftTUI/Window/Window.swift
+++ Sources/SwiftTUI/Window/Window.swift
@@ -6,7 +6,64 @@
 //
 
 import Foundation
+import cncurses
 
 public class Window {
+    // NOTE: ncurses root object name is `SCREEN`.
+    // But defined type name is `WINDOW`.
+    internal typealias _Widnow = UnsafeMutablePointer<cncurses.WINDOW>
+    // NOTE: Keep screen
+    internal var window: _Widnow!
     
+    internal init() {
+        window = initscr()
+    }
+}
+
+// MARK - Named ncurses functions
+private extension Window {
+    func enableNoBufferingMode() {
+        // NOTE: Configure about immediately receive input keyevent
+        cbreak()
+    }
+    func setupNoEchoOnWindow() {
+        noecho()
+    }
+    func enableCursor() {
+        let enable: Int32 = 1
+        curs_set(enable)
+    }
+    func callUseColor() {
+        start_color()
+    }
+    func configureColorPairs() {
+        // NOTE: Range of 1 ~ (COLOR_PAIRS-1). If set 0, Setting foreground color is white and background color is black.
+        // And It can not changed color pair.
+        let pairNumber: Int16 = 1
+        init_pair(pairNumber, Int16(COLOR_BLACK), Int16(COLOR_WHITE))
+    }
+    func setupKeypad() {
+        // NOTE: If you set false, return character with escape sequence when input with function key
+        let returnRawKeyCode = true
+        keypad(window, returnRawKeyCode)
+    }
+}
+
+// MARK: - Internal Named ncurses functions
+internal extension Window {
+    func setup() {
+        enableNoBufferingMode()
+        setupNoEchoOnWindow()
+        callUseColor()
+        enableCursor()
+        configureColorPairs()
+        setupKeypad()
+        clear();
+    }
+    func dispose() {
+        if !isendwin() {
+            endwin()
+        }
+        delscreen(OpaquePointer(window))
+    }
 }
