b83f565b44207b111ec3eaf109e21445c07c2bb3
bannzai <kingkong999yhirose@gmail.com>
2019-12-14 22:32:47 +0900

    :recycle: Fix cycle references

--- Sources/SwiftTUI/Cursor/Cursor.swift
+++ Sources/SwiftTUI/Cursor/Cursor.swift
@@ -12,7 +12,7 @@ public struct Cursor {
     internal var x: PhysicalDistance = 0
     internal var y: PhysicalDistance = 0
     
-    internal var screen: Screen
+    internal weak var screen: Screen?
     
     internal init(screen: Screen) {
         self.screen = screen
@@ -20,14 +20,14 @@ public struct Cursor {
 }
 
 internal extension Cursor {
-    func moveTo(x: PhysicalDistance, y: PhysicalDistance) {
-        screen.cursor.x = x
-        screen.cursor.y = y
+    mutating func moveTo(x: PhysicalDistance, y: PhysicalDistance) {
+        self.x = x
+        self.y = y
         cncurses.move(Int32(x), Int32(y))
     }
-    func move(x: PhysicalDistance, y: PhysicalDistance) {
-        let _x = screen.cursor.x + x
-        let _y = screen.cursor.y + y
+    mutating func move(x: PhysicalDistance, y: PhysicalDistance) {
+        let _x = self.x + x
+        let _y = self.y + y
         moveTo(x: _x, y: _y)
     }
 }
