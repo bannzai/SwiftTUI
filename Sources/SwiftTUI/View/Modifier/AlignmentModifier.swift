0be7fdf4a2575fbbe07580e083d654d695b06d6d
bannzai <kingkong999yhirose@gmail.com>
2019-12-29 19:48:05 +0900

    :sparkels: add _AlignmentWritingModifier

---
+++ Sources/SwiftTUI/View/Modifier/AlignmentModifier.swift
@@ -0,0 +1,32 @@
+//
+//  AlignmentModifier.swift
+//  SwiftTUI
+//
+//  Created by Yudai.Hirose on 2019/12/29.
+//
+
+import Foundation
+
+@frozen public struct _AlignmentWritingModifier : ViewModifier {
+    @usableFromInline
+    internal let key: AlignmentKey
+    @usableFromInline
+    internal let computeValue: (ViewDimensions) -> PhysicalDistance
+    @inlinable internal init(key: AlignmentKey, computeValue: @escaping (ViewDimensions) -> PhysicalDistance) {
+        self.key = key
+        self.computeValue = computeValue
+    }
+    public typealias Body = Swift.Never
+}
+
+extension View {
+    @inlinable public func alignmentGuide(_ g: HorizontalAlignment, computeValue: @escaping (ViewDimensions) -> PhysicalDistance) -> some View {
+          return modifier(
+              _AlignmentWritingModifier(key: g.key, computeValue: computeValue))
+      }
+    
+    @inlinable public func alignmentGuide(_ g: VerticalAlignment, computeValue: @escaping (ViewDimensions) -> PhysicalDistance) -> some View {
+          return modifier(
+              _AlignmentWritingModifier(key: g.key, computeValue: computeValue))
+      }
+}
