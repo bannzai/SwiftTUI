de70e88d5d4a8289709b0e9ee060fd9b85642561
bannzai <kingkong999yhirose@gmail.com>
2019-12-30 01:43:35 +0900

    :sparkles: Implement reduce content width function

--- Sources/SwiftTUI/Core/SwiftTUIConentType.swift
+++ Sources/SwiftTUI/Core/SwiftTUIConentType.swift
@@ -6,6 +6,39 @@
 //
 
 import Foundation
+import cncurses
 
 // TODO: Internal
 public typealias SwiftTUIContentType = String
+
+// MARK: - Size
+extension SwiftTUIContentType {
+    internal var width: PhysicalDistance {
+        map { $0.width }.reduce(0, +)
+    }
+}
+
+fileprivate extension Character {
+    static var cache: [Self: PhysicalDistance] = [:]
+    func doCache(width: PhysicalDistance) -> PhysicalDistance {
+        Self.cache[self] = width
+        return width
+    }
+    var width: PhysicalDistance {
+        if let cached = Self.cache[self] {
+            return cached
+        }
+        if let ascii = asciiValue {
+            switch ascii < 32 {
+            case true:
+                return doCache(width: 0)
+            case false:
+                return doCache(width: 1)
+            }
+        }
+        let width = unicodeScalars
+            .map { cncurses.wcwidth(Int32($0.value)) }
+            .reduce(0) { max($0, $1) }
+        return doCache(width: Int(width))
+    }
+}
