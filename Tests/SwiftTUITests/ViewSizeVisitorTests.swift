cb191a9e5baf6c68ae7faffeaea397aa35702ba8
bannzai <kingkong999yhirose@gmail.com>
2020-01-06 21:09:33 +0900

    :sparkles: Add failure test case

--- Tests/SwiftTUITests/ViewSizeVisitorTests.swift
+++ Tests/SwiftTUITests/ViewSizeVisitorTests.swift
@@ -104,6 +104,20 @@ class ViewSizeVisitorTests: XCTestCase {
             
             XCTAssertEqual(result, Size(width: "123".width, height: 1))
         }
+        XCTContext.runActivity(named: "when CustomView has VStack<TupleView<Text, Text, Text>>") { (_) in
+            let view = CustomView(body: VStack {
+                Text("123")
+                Text("456")
+                Text("789")
+            })
+            
+            let graphVisitor = ViewGraphSetVisitor()
+            let graph = view.accept(visitor: graphVisitor)
+            let sizeVisitor = ViewSizeVisitor()
+            let result = graph.accept(visitor: sizeVisitor)
+            
+            XCTAssertEqual(result, Size(width: "123".width, height: 1))
+        }
     }
 
     func testPerformanceExample() {
