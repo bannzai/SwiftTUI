72c75e61f3096f6cb064dfa5565e87794cb4797e
bannzai <kingkong999yhirose@gmail.com>
2020-01-10 11:27:03 +0900

    :sparkles: Add test case of linebreak Text

--- Tests/SwiftTUITests/ViewDimensionsVisitor.swift
+++ Tests/SwiftTUITests/ViewDimensionsVisitor.swift
@@ -39,10 +39,31 @@ class ViewDimensionsVisitorTests: XCTestCase {
 
             XCTAssertEqual(dimensions.width, "hoge".width)
             XCTAssertEqual(dimensions.height, 1)
-            XCTAssertEqual(dimensions[HorizontalAlignment.center], "hoge".width / 2)
-            XCTAssertEqual(dimensions[VerticalAlignment.center], 0)
-            XCTAssertNil(dimensions[explicit: HorizontalAlignment.center])
-            XCTAssertNil(dimensions[explicit: VerticalAlignment.center])
+            XCTAssertEqual(dimensions[HorizontalAlignment.default], "hoge".width / 2)
+            XCTAssertEqual(dimensions[VerticalAlignment.default], 0)
+            XCTAssertNil(dimensions[explicit: HorizontalAlignment.default])
+            XCTAssertNil(dimensions[explicit: VerticalAlignment.default])
+        }
+        
+        XCTContext.runActivity(named: "when Text with content with linebreak") { (_) in
+            let view = Text("hoge\nfuga")
+
+            let graphVisitor = ViewGraphSetVisitor()
+            let graph = graphVisitor.visit(view: view)
+            
+            // FIXME: Remove Size Visitor??
+            let sizeVisitor = ViewSizeVisitor()
+            _ = sizeVisitor.visit(graph)
+            
+            let visitor = ViewDimensionsVisitor()
+            let dimensions = visitor.visit(graph)
+            
+            XCTAssertEqual(dimensions.width, "hoge".width)
+            XCTAssertEqual(dimensions.height, 2)
+            XCTAssertEqual(dimensions[HorizontalAlignment.default], "hoge".width / 2)
+            XCTAssertEqual(dimensions[VerticalAlignment.default], 1)
+            XCTAssertNil(dimensions[explicit: HorizontalAlignment.default])
+            XCTAssertNil(dimensions[explicit: VerticalAlignment.default])
         }
     }
 
