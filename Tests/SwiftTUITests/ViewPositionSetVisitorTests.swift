910761868e93094a73c811872e6bfec9f0aca217
bannzai <kingkong999yhirose@gmail.com>
2020-03-22 22:44:16 +0900

    :recycle: add test case for reversible alignments

--- Tests/SwiftTUITests/ViewPositionSetVisitorTests.swift
+++ Tests/SwiftTUITests/ViewPositionSetVisitorTests.swift
@@ -298,7 +298,7 @@ class ViewPositionSetVisitorTests: XCTestCase {
                 }
             }
         }
-        XCTContext.runActivity(named: "when VStack contains TupleView<Text, Text, ModifiedContent<Text, _AlignmentWritingModifier>> when .trailing alignment. And configure alignmentGuide") { (_) in
+        XCTContext.runActivity(named: "when VStack contains TupleView<Text(\"1\"), Text(\"23\"), ModifiedContent<Text(\"456\"), _AlignmentWritingModifier>> when .trailing alignment. And configure alignmentGuide") { (_) in
             let view = VStack(alignment: .trailing) {
                 Text("1")
                 Text("23")
@@ -394,6 +394,59 @@ class ViewPositionSetVisitorTests: XCTestCase {
                     
                     XCTAssertEqual(text.content, "1")
                     XCTAssertEqual(textGraph.rect.origin.x, 2)
+                    XCTAssertEqual(textGraph.rect.origin.y, 5)
+                }
+            }
+        }
+        XCTContext.runActivity(named: "when VStack contains TupleView<ModifiedContent<Text(\"1\"), _AlignmentWritingModifier>, Text(\"23\"), Text(\"456\")> when .trailing alignment. And configure alignmentGuide") { (_) in
+            let view = VStack(alignment: .trailing) {
+                Text("456")
+                    .alignmentGuide(.trailing, computeValue: { _ in return 2 })
+                Text("23")
+                Text("1")
+            }
+            
+            let graph = prepare(view: view)
+            let visitor = ViewPositionSetVisitor()
+            visitor.visit(graph)
+            
+            XCTAssertEqual(graph.rect.origin.x, 0)
+            XCTAssertEqual(graph.rect.origin.y, 0)
+            
+            XCTContext.runActivity(named: "Child graph confirm to trailing position") { (_) in
+                first: do {
+                    let modifierGraph = graph.children[0].children[0]
+                    let hasModifier = modifierGraph.anyView as! HasAnyModifier
+                    XCTAssertTrue(hasModifier.anyModifier is _AlignmentWritingModifier)
+                    
+                    let textGraph = modifierGraph.children[0]
+                    XCTAssertTrue(textGraph.anyView is Text)
+                    let text = textGraph.anyView as! Text
+                    
+                    XCTAssertEqual(text.content, "456")
+                    XCTAssertEqual(textGraph.rect.origin.x, 0)
+                    XCTAssertEqual(textGraph.rect.origin.y, 0)
+                    XCTAssertEqual(modifierGraph.rect.origin.x, 0)
+                    XCTAssertEqual(modifierGraph.rect.origin.y, 0)
+                }
+                second: do {
+                    let textGraph = graph.children[0].children[1]
+                    
+                    XCTAssertTrue(textGraph.anyView is Text)
+                    let text = textGraph.anyView as! Text
+                    
+                    XCTAssertEqual(text.content, "23")
+                    XCTAssertEqual(textGraph.rect.origin.x, 0)
+                    XCTAssertEqual(textGraph.rect.origin.y, 3)
+                }
+                third: do {
+                    let textGraph = graph.children[0].children[2]
+                    
+                    XCTAssertTrue(textGraph.anyView is Text)
+                    let text = textGraph.anyView as! Text
+                    
+                    XCTAssertEqual(text.content, "1")
+                    XCTAssertEqual(textGraph.rect.origin.x, 1)
                     XCTAssertEqual(textGraph.rect.origin.y, 5)
                 }
             }
