913450299b6e09553e5172e7d60b7099ad5f8153
bannzai <kingkong999yhirose@gmail.com>
2020-03-30 21:59:17 +0900

    Revert ":sparkels: using typealias for old visitors"
    This reverts commit 3f002a94b56c1ea782320d032b4db7b2c029af00.

--- Sources/SwiftTUI/View/ViewGraph/ViewDimensionsVisitor.swift
+++ Sources/SwiftTUI/View/ViewGraph/ViewDimensionsVisitor.swift
@@ -8,23 +8,22 @@
 import Foundation
 
 internal protocol ViewDimensionsAcceptable {
-//    func accept(visitor: ViewDimensionsVisitor) -> ViewDimensionsVisitor.VisitResult
+    func accept(visitor: ViewDimensionsVisitor) -> ViewDimensionsVisitor.VisitResult
 }
 
-typealias ViewDimensionsVisitor = ViewIntrinsicContentSizeVisitor
-//internal final class ViewDimensionsVisitor: Visitor {
-//    internal typealias VisitResult = Void
-//    internal init() { }
-//
-//    internal var currentContainerGraph: ViewGraph?
-//
-//    internal func visit<T: View>(_ content: T) -> VisitResult {
-//        debugLogger.debug()
-//        switch content {
-//        case let acceptable as ViewDimensionsAcceptable:
-//            return acceptable.accept(visitor: self)
-//        case _:
-//            return visit(content.body)
-//        }
-//    }
-//}
+internal final class ViewDimensionsVisitor: Visitor {
+    internal typealias VisitResult = Void
+    internal init() { }
+    
+    internal var currentContainerGraph: ViewGraph?
+
+    internal func visit<T: View>(_ content: T) -> VisitResult {
+        debugLogger.debug()
+        switch content {
+        case let acceptable as ViewDimensionsAcceptable:
+            return acceptable.accept(visitor: self)
+        case _:
+            return visit(content.body)
+        }
+    }
+}
--- Sources/SwiftTUI/View/ViewGraph/ViewPositionSetVisitor.swift
+++ Sources/SwiftTUI/View/ViewGraph/ViewPositionSetVisitor.swift
@@ -8,21 +8,20 @@
 import Foundation
 
 internal protocol ViewPositionSetterAcceptable {
-//    func accept(visitor: ViewPositionSetVisitor) -> ViewPositionSetVisitor.VisitResult
+    func accept(visitor: ViewPositionSetVisitor) -> ViewPositionSetVisitor.VisitResult
 }
 
-typealias ViewPositionSetVisitor = ViewIntrinsicContentSizeVisitor
-//internal final class ViewPositionSetVisitor: Visitor {
-//    internal typealias VisitResult = Void
-//    internal init() { }
-//
-//    internal func visit<T: View>(_ content: T) -> VisitResult {
-//        debugLogger.debug()
-//        switch content {
-//        case let acceptable as ViewPositionSetterAcceptable:
-//            return acceptable.accept(visitor: self)
-//        case _:
-//            return visit(content.body)
-//        }
-//    }
-//}
+internal final class ViewPositionSetVisitor: Visitor {
+    internal typealias VisitResult = Void
+    internal init() { }
+    
+    internal func visit<T: View>(_ content: T) -> VisitResult {
+        debugLogger.debug()
+        switch content {
+        case let acceptable as ViewPositionSetterAcceptable:
+            return acceptable.accept(visitor: self)
+        case _:
+            return visit(content.body)
+        }
+    }
+}
