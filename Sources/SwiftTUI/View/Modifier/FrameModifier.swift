c667435b16ecef9ae0845fc528a79df0293d2b7c
bannzai <kingkong999yhirose@gmail.com>
2020-04-01 09:11:28 +0900

    :sparkles: add alignment

--- Sources/SwiftTUI/View/Modifier/FrameModifier.swift
+++ Sources/SwiftTUI/View/Modifier/FrameModifier.swift
@@ -10,15 +10,16 @@ import Foundation
 @frozen public struct _FrameLayout: ViewModifier {
     internal let width: PhysicalDistance?
     internal let height: PhysicalDistance?
+    internal let alignment: Alignment?
     public typealias Body = Swift.Never
     
     internal var _baseProperty: _ViewBaseProperties = _ViewBaseProperties()
 }
 
 extension View {
-    public func frame(width: PhysicalDistance? = nil, height: PhysicalDistance? = nil) -> some View {
+    public func frame(width: PhysicalDistance? = nil, height: PhysicalDistance? = nil, alignment: Alignment? = nil) -> some View {
         modifier(
-            _FrameLayout(width: width, height: height)
+            _FrameLayout(width: width, height: height, alignment: alignment)
         )
     }
     
