2db125a09d6537735a1b8aec84ecf88b3fc19f02
bannzai <kingkong999yhirose@gmail.com>
2020-04-03 20:33:03 +0900

    Revert ":recycle: fix demo for VStack complex pattern"
    This reverts commit 8ef2c4be84092a199258f538a5252958b3499a12.

--- Sources/Demo/main.swift
+++ Sources/Demo/main.swift
@@ -7,9 +7,21 @@ struct ContentView: View {
     var body: some View {
         VStack(alignment: .leading) {
             Text("Hello")
+                .alignmentGuide(.leading, computeValue: { _ in return 1 })
+                .alignmentGuide(.leading, computeValue: { d in d[explicit: .leading]! + 2 })
+                .background(Color.red)
+            Text(",")
+            Text("World")
+                .padding(2)
+            
             VStack(alignment: .trailing) {
+                Text("Hello")
+                    .foregroundColor(.yellow)
                 Text(",")
+                    .background(Color.blue)
                 Text("World")
+                    .alignmentGuide(.leading, computeValue: { _ in return 10 })
+                    .alignmentGuide(.trailing, computeValue: { d in d[explicit: .leading]! + 2 })
             }
         }
     }
