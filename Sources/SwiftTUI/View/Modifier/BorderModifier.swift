7476d1370f3a40f38b5323b300375358159e273f
bannzai <kingkong999yhirose@gmail.com>
2020-04-04 10:04:23 +0900

    :recycle: fix border modifiers

--- Sources/SwiftTUI/View/Modifier/BorderModifier.swift
+++ Sources/SwiftTUI/View/Modifier/BorderModifier.swift
@@ -8,12 +8,14 @@
 import Foundation
 
 public struct Border {
-    internal var color: Color
-    internal var width: PhysicalDistance
-    internal var directionType: DirectionType
-    
-    public init(color: Color, width: PhysicalDistance, directionType: DirectionType) {
-        self.color = color
+    public let color: Color
+    public let delimiter: String?
+    public let width: PhysicalDistance
+    public let directionType: DirectionType
+
+    public init(color: Color?, delimiter: String?, width: PhysicalDistance, directionType: DirectionType) {
+        self.color = color ?? Style.Color.border.color
+        self.delimiter = delimiter
         self.width = width
         self.directionType = directionType
     }
@@ -26,23 +28,17 @@ public struct Border {
     }
 }
 
-@frozen public struct _BorderModifier<Target>: ViewModifier where Target: View {
-    public let target: Target
-    public let border: Border
-    @inlinable public init(target: Target, border: Border) {
-        self.target = target
+@frozen public struct _BorderModifier: ViewModifier {
+    @usableFromInline internal let border: Border
+    
+    public init(border: Border) {
         self.border = border
     }
     public typealias Body = Swift.Never
 }
 
-extension _BorderModifier: Swift.Equatable where Target: Swift.Equatable {
-    public static func == (lhs: _BorderModifier<Target>, rhs: _BorderModifier<Target>) -> Swift.Bool {
-        lhs.target == rhs.target
-    }
-}
 extension View {
-    @inlinable public func border<S>(color: Color, width: PhysicalDistance = 1, direction: Border.DirectionType = .default) -> some View {
-        modifier(_BorderModifier(target: self, border: Border(color: color, width: width, directionType: direction)))
+    @inlinable public func border(color: Color? = nil, delimiter: String? = nil, width: PhysicalDistance = 1, direction: Border.DirectionType = .default) -> some View {
+        modifier(_BorderModifier(border: Border(color: color, delimiter: delimiter, width: width, directionType: direction)))
     }
 }
