813b8d9488d32bb40fc8ed694eb44935fc743333
bannzai <kingkong999yhirose@gmail.com>
2020-04-04 16:41:36 +0900

    :recycle: add test case about border

--- Tests/SwiftTUITests/ViewGraphTests.swift
+++ Tests/SwiftTUITests/ViewGraphTests.swift
@@ -10,16 +10,28 @@ import XCTest
 
 class ViewGraphTests: XCTestCase {
     func testPositionToWindow() {
-        let parentGraph = ViewGraphImpl(view: EmptyView())
-        parentGraph.rect.origin.x = 10
-        parentGraph.rect.origin.y = 15
-        
-        let graph = ViewGraphImpl(view: EmptyView())
-        graph.rect.origin.x = 10
-        graph.rect.origin.y = 15
-        
-        parentGraph.addChild(graph)
+        XCTContext.runActivity(named: "simply tests") { _ in
+            let parentGraph = ViewGraphImpl(view: EmptyView())
+            parentGraph.rect.origin.x = 10
+            parentGraph.rect.origin.y = 15
+            
+            let graph = ViewGraphImpl(view: EmptyView())
+            graph.rect.origin.x = 10
+            graph.rect.origin.y = 15
+            
+            parentGraph.addChild(graph)
+            
+            XCTAssertEqual(graph.positionToWindow(), Point(x: 20, y: 30))
+        }
+        XCTContext.runActivity(named: "when Text.border()") { _ in
+            let view = Text("123").border()
+            let graph = ViewGraphSetVisitor().visit(view)
+            graph.accept(visitor: .init())
+            
+            let childGraph = graph.children[0]
+            XCTAssertTrue(childGraph.anyView is Text)
 
-        XCTAssertEqual(graph.positionToWindow(), Point(x: 20, y: 30))
+            XCTAssertEqual(childGraph.positionToWindow(), Point(x: 1, y: 1))
+        }
     }
 }
