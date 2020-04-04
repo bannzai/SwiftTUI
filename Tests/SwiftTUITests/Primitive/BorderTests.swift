cab15c26e0e2ff1c25c23823bd3398d291e2b415
bannzai <kingkong999yhirose@gmail.com>
2020-04-04 10:03:39 +0900

    :memo: add failure test case about border modifier

---
+++ Tests/SwiftTUITests/Primitive/BorderTests.swift
@@ -0,0 +1,35 @@
+//
+//  BorderTests.swift
+//  SwiftTUITests
+//
+//  Created by Yudai.Hirose on 2020/04/04.
+//
+
+import XCTest
+@testable import SwiftTUI
+
+fileprivate let defaultBorderWidth = 1
+class BorderTests: XCTestCase {
+    override func setUp() {
+        super.setUp()
+        
+        mainScreen = DummyScreen.init()
+    }
+    
+    func testSize() throws {
+        func prepare<T: View>(view: T) -> ViewGraph {
+            let graphVisitor = ViewGraphSetVisitor()
+            let graph = graphVisitor.visit(view)
+            return graph
+        }
+        XCTContext.runActivity(named: "when call border()") { (_) in
+            let view = Text("123").border()
+            
+            let graph = prepare(view: view)
+            let visitor = ViewSetRectVisitor()
+            graph.accept(visitor: visitor)
+            
+            XCTAssertEqual(graph.rect.size, Size(width: "123".width + defaultBorderWidth * 2, height: "123".height + defaultBorderWidth * 2))
+        }
+    }
+}
