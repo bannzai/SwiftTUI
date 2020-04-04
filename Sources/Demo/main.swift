e98db90a3e0f2b3f16448eb1739ca2117e78ae50
bannzai <kingkong999yhirose@gmail.com>
2020-04-05 02:23:18 +0900

    :memo: using border function

--- Sources/Demo/main.swift
+++ Sources/Demo/main.swift
@@ -5,25 +5,7 @@ import Runtime
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Hello")
-                .alignmentGuide(.leading, computeValue: { _ in return 1 })
-                .alignmentGuide(.leading, computeValue: { d in d[explicit: .leading]! + 2 })
-                .background(Color.red)
-            Text(",")
-            Text("World")
-                .padding(2)
-            
-            VStack(alignment: .trailing) {
-                Text("Hello")
-                    .foregroundColor(.yellow)
-                Text(",")
-                    .background(Color.blue)
-                Text("World")
-                    .alignmentGuide(.leading, computeValue: { _ in return 10 })
-                    .alignmentGuide(.trailing, computeValue: { d in d[explicit: .leading]! + 2 })
-            }
-        }
+        Text("123").border()
     }
 }
 let view = ContentView()
