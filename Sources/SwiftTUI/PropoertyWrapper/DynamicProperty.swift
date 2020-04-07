24cf89ac45b34f93fe6a8c881246b1ef9c427033
bannzai <kingkong999yhirose@gmail.com>
2020-04-07 13:53:51 +0900

    :sparkles: Add DynamicProperty

---
+++ Sources/SwiftTUI/PropoertyWrapper/DynamicProperty.swift
@@ -0,0 +1,18 @@
+//
+//  DynamicProperty.swift
+//  SwiftTUI
+//
+//  Created by Yudai.Hirose on 2020/04/06.
+//
+
+import Foundation
+
+public protocol DynamicProperty {
+    mutating func update()
+}
+
+extension DynamicProperty {
+    mutating public func update() {
+        
+    }
+}
