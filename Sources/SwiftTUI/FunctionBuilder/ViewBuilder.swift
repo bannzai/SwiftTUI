6d6dbe638c43d08a147aba3333e633907466193c
bannzai <kingkong999yhirose@gmail.com>
2020-04-08 09:09:05 +0900

    :sparkles: implement if else set graph visit

--- Sources/SwiftTUI/FunctionBuilder/ViewBuilder.swift
+++ Sources/SwiftTUI/FunctionBuilder/ViewBuilder.swift
@@ -68,3 +68,22 @@ extension ViewBuilder {
     public static func buildBlock<C0, C1, C2, C3, C4, C5, C6, C7, C8>(_ c0: C0, _ c1: C1, _ c2: C2, _ c3: C3, _ c4: C4, _ c5: C5, _ c6: C6, _ c7: C7, _ c8: C8) -> TupleView<(C0, C1, C2, C3, C4, C5, C6, C7, C8)> where C0 : View, C1 : View, C2 : View, C3 : View, C4 : View, C5 : View, C6 : View, C7 : View, C8 : View { TupleView((c0, c1, c2, c3, c4, c5, c6, c7, c8)) }
     public static func buildBlock<C0, C1, C2, C3, C4, C5, C6, C7, C8, C9>(_ c0: C0, _ c1: C1, _ c2: C2, _ c3: C3, _ c4: C4, _ c5: C5, _ c6: C6, _ c7: C7, _ c8: C8, _ c9: C9) -> TupleView<(C0, C1, C2, C3, C4, C5, C6, C7, C8, C9)> where C0 : View, C1 : View, C2 : View, C3 : View, C4 : View, C5 : View, C6 : View, C7 : View, C8 : View, C9 : View { TupleView((c0, c1, c2, c3, c4, c5, c6, c7, c8, c9)) }
 }
+
+extension ViewBuilder._ConditionalContent: ViewGraphSetAcceptable {
+    private func contentViewGraph(visitor: ViewGraphSetVisitor) -> ViewGraph {
+        switch storage {
+        case .truthy(let view):
+            return visitor.visit(view)
+        case .falsy(let view):
+            return visitor.visit(view)
+        }
+    }
+    func accept(visitor: ViewGraphSetVisitor) -> ViewGraph {
+        let graph = ViewGraphImpl(view: self)
+        let keepCurrent = visitor.current
+        defer { visitor.current = keepCurrent }
+        visitor.current = graph
+        graph.addChild(contentViewGraph(visitor: visitor))
+        return graph
+    }
+}
