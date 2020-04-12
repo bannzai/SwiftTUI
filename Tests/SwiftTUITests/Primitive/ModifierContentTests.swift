f09145ea0f9f9302f15cb26e4576c87974d25340
bannzai <kingkong999yhirose@gmail.com>
2020-04-12 14:32:42 +0900

    :sparkles: add failure test case about border modifier size

---
+++ Tests/SwiftTUITests/Primitive/ModifierContentTests.swift
@@ -0,0 +1,34 @@
+//
+//  ModifierContentTests.swift
+//  SwiftTUITests
+//
+//  Created by Yudai.Hirose on 2020/04/12.
+//
+
+import XCTest
+@testable import SwiftTUI
+
+class ModifierContentTests: XCTestCase {
+    override func setUp() {
+        super.setUp()
+        
+        mainScreen = DummyScreen.init()
+    }
+
+    func testSize() throws {
+        XCTContext.runActivity(named: "with border modifier") { _ in
+            struct BorderModifier: ViewModifier {
+                func body(content: Content) -> some View {
+                    content.border(.red)
+                }
+            }
+            let view = Text("text").modifier(BorderModifier())
+            
+            let graph = prepareViewGraph(view: view)
+            graph.accept(visitor: ViewSetRectVisitor())
+            
+            XCTAssertEqual(graph.rect.size, Size(width: "text".width + 2, height: "text".height + 1))
+        }
+    }
+
+}
