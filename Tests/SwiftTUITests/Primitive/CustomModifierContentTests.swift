4184986a8b56cf5acd79e4eb0fdb0a7bd2d02014
bannzai <kingkong999yhirose@gmail.com>
2020-04-12 21:20:30 +0900

    :sparkles: add test case about consecutive modifiers

--- Tests/SwiftTUITests/Primitive/CustomModifierContentTests.swift
+++ Tests/SwiftTUITests/Primitive/CustomModifierContentTests.swift
@@ -33,6 +33,36 @@ class CustomModifierContentTests: XCTestCase {
             XCTAssertTrue(wrappedModifier.anyView is ModifiedContent<_ViewModifier_Content<Modifier>, _BorderModifier>)
             XCTAssertEqual(wrappedModifier.rect.size, Size(width: "text".width + 2, height: "text".height + 2))
         }
+        
     }
+    
+    func test_playground() {
+        XCTContext.runActivity(named: "with border.border.border modifier") { _ in
+            struct Modifier: ViewModifier {
+                func body(content: Content) -> some View {
+                    content.border(.red).border(.yellow).border(.blue)
+                }
+            }
+            let view = Text("text").modifier(Modifier())
+
+            let graph = prepareViewGraph(view: view)
+            graph.accept(visitor: ViewSetRectVisitor())
+            XCTAssertTrue(graph.anyView is ModifiedContent<Text, Modifier>)
+            XCTAssertEqual(graph.rect.size, Size(width: "text".width + 2 + 2 + 2, height: "text".height + 2 + 2 + 2))
+
+            let blue = graph.children[0]
+            XCTAssertTrue(blue.anyView is ModifiedContent<ModifiedContent<ModifiedContent<_ViewModifier_Content<Modifier>, _BorderModifier>, _BorderModifier>, _BorderModifier>)
+            XCTAssertEqual(blue.rect.size, Size(width: "text".width + 2 + 2 + 2, height: "text".height + 2 + 2 + 2))
+            
+            let yellow = blue.children[0]
+            XCTAssertTrue(yellow.anyView is ModifiedContent<ModifiedContent<_ViewModifier_Content<Modifier>, _BorderModifier>, _BorderModifier>)
+            XCTAssertEqual(yellow.rect.size, Size(width: "text".width + 2 + 2, height: "text".height + 2 + 2))
+            
+            let red = yellow.children[0]
+            XCTAssertTrue(red.anyView is ModifiedContent<_ViewModifier_Content<Modifier>, _BorderModifier>)
+            XCTAssertEqual(red.rect.size, Size(width: "text".width + 2, height: "text".height + 2))
+        }
+    }
+    
 
 }
