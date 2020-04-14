b84a529628396fa68d6da2b04edad0234b33e86d
bannzai <kingkong999yhirose@gmail.com>
2020-04-15 00:15:10 +0900

    :sparkles: Implement ForEach initializer

--- Sources/SwiftTUI/View/List/ForEach.swift
+++ Sources/SwiftTUI/View/List/ForEach.swift
@@ -16,12 +16,21 @@ extension ForEach: View {
     public typealias Body = Swift.Never
 }
 extension ForEach where ID == Data.Element.ID, Content: View, Data.Element: Swift.Identifiable {
-    public init(_ data: Data, @ViewBuilder content: @escaping (Data.Element) -> Content) { fatalError() }
+    public init(_ data: Data, @ViewBuilder content: @escaping (Data.Element) -> Content) {
+        self.data = data
+        self.content = content
+    }
 }
 extension ForEach where Content: View {
-    public init(_ data: Data, id: Swift.KeyPath<Data.Element, ID>, content: @escaping (Data.Element) -> Content) { fatalError() }
+    public init(_ data: Data, id: Swift.KeyPath<Data.Element, ID>, content: @escaping (Data.Element) -> Content) {
+        self.data = data
+        self.content = content
+    }
 }
 extension ForEach where Data == Swift.Range<Swift.Int>, ID == Swift.Int, Content: View {
-    public init(_ data: Swift.Range<Swift.Int>, @ViewBuilder content: @escaping (Swift.Int) -> Content) { fatalError() }
+    public init(_ data: Swift.Range<Swift.Int>, @ViewBuilder content: @escaping (Swift.Int) -> Content) {
+        self.data = data
+        self.content = content
+    }
 }
 
