2fc95bbd87610743ae7bf9f45294778d11d5e10d
bannzai <kingkong999yhirose@gmail.com>
2020-04-15 23:41:36 +0900

    :memo: Fix List initializers

--- Sources/SwiftTUI/View/List/List.swift
+++ Sources/SwiftTUI/View/List/List.swift
@@ -8,20 +8,71 @@
 import Foundation
 
 public struct List<SelectionValue, Content>: View where SelectionValue: Swift.Hashable, Content: View {
-    public init(selection: Binding<Swift.Set<SelectionValue>>?, @ViewBuilder content: () -> Content) { fatalError() }
-    public init(selection: Binding<SelectionValue?>?, @ViewBuilder content: () -> Content) { fatalError() }
+    internal var selection: Binding<SelectionValue?>?
+    internal var selectionSet: Binding<Swift.Set<SelectionValue>>?
+    internal var content: Content
+    public init(selection: Binding<Swift.Set<SelectionValue>>?, @ViewBuilder content: () -> Content) {
+        self.selectionSet = selection
+        self.content = content()
+    }
+    public init(selection: Binding<SelectionValue?>?, @ViewBuilder content: () -> Content) {
+        self.selection = selection
+        self.content = content()
+    }
     public var body: some View {
         return fatalError()
     }
 //    public typealias Body = @_opaqueReturnTypeOf("$s7SwiftUI4ListV4bodyQrvp", 0) 🦸<SelectionValue, Content>
 }
 extension List {
-    public init<Data, RowContent>(_ data: Data, selection: Binding<Swift.Set<SelectionValue>>?, @ViewBuilder rowContent: @escaping (Data.Element) -> RowContent) where Content == ForEach<Data, Data.Element.ID, HStack<RowContent>>, Data: Swift.RandomAccessCollection, RowContent: View, Data.Element: Swift.Identifiable { fatalError() }
-    public init<Data, ID, RowContent>(_ data: Data, id: Swift.KeyPath<Data.Element, ID>, selection: Binding<Swift.Set<SelectionValue>>?, @ViewBuilder rowContent: @escaping (Data.Element) -> RowContent) where Content == ForEach<Data, ID, HStack<RowContent>>, Data: Swift.RandomAccessCollection, ID: Swift.Hashable, RowContent: View { fatalError() }
-    public init<RowContent>(_ data: Swift.Range<Swift.Int>, selection: Binding<Swift.Set<SelectionValue>>?, @ViewBuilder rowContent: @escaping (Swift.Int) -> RowContent) where Content == ForEach<Swift.Range<Swift.Int>, Swift.Int, HStack<RowContent>>, RowContent: View { fatalError() }
-    public init<Data, RowContent>(_ data: Data, selection: Binding<SelectionValue?>?, @ViewBuilder rowContent: @escaping (Data.Element) -> RowContent) where Content == ForEach<Data, Data.Element.ID, HStack<RowContent>>, Data: Swift.RandomAccessCollection, RowContent: View, Data.Element: Swift.Identifiable { fatalError() }
-    public init<Data, ID, RowContent>(_ data: Data, id: Swift.KeyPath<Data.Element, ID>, selection: Binding<SelectionValue?>?, @ViewBuilder rowContent: @escaping (Data.Element) -> RowContent) where Content == ForEach<Data, ID, HStack<RowContent>>, Data: Swift.RandomAccessCollection, ID: Swift.Hashable, RowContent: View { fatalError() }
-    public init<RowContent>(_ data: Swift.Range<Swift.Int>, selection: Binding<SelectionValue?>?, @ViewBuilder rowContent: @escaping (Swift.Int) -> RowContent) where Content == ForEach<Swift.Range<Swift.Int>, Swift.Int, HStack<RowContent>>, RowContent: View { fatalError() }
+    public init<Data, RowContent>(_ data: Data, selection: Binding<Swift.Set<SelectionValue>>?, @ViewBuilder rowContent: @escaping (Data.Element) -> RowContent) where Content == ForEach<Data, Data.Element.ID, HStack<RowContent>>, Data: Swift.RandomAccessCollection, RowContent: View, Data.Element: Swift.Identifiable {
+        self.selectionSet = selection
+        self.content = ForEach.init(data) { element in
+            HStack {
+                rowContent(element)
+            }
+        }
+    }
+    public init<Data, ID, RowContent>(_ data: Data, id: Swift.KeyPath<Data.Element, ID>, selection: Binding<Swift.Set<SelectionValue>>?, @ViewBuilder rowContent: @escaping (Data.Element) -> RowContent) where Content == ForEach<Data, ID, HStack<RowContent>>, Data: Swift.RandomAccessCollection, ID: Swift.Hashable, RowContent: View {
+        self.selectionSet = selection
+        self.content = ForEach.init(data, id: id, content: { element in
+            HStack {
+                rowContent(element)
+            }
+        })
+    }
+    public init<RowContent>(_ data: Swift.Range<Swift.Int>, selection: Binding<Swift.Set<SelectionValue>>?, @ViewBuilder rowContent: @escaping (Swift.Int) -> RowContent) where Content == ForEach<Swift.Range<Swift.Int>, Swift.Int, HStack<RowContent>>, RowContent: View {
+        self.selectionSet = selection
+        self.content = ForEach.init(data) { (element) in
+            HStack {
+                rowContent(element)
+            }
+        }
+    }
+    public init<Data, RowContent>(_ data: Data, selection: Binding<SelectionValue?>?, @ViewBuilder rowContent: @escaping (Data.Element) -> RowContent) where Content == ForEach<Data, Data.Element.ID, HStack<RowContent>>, Data: Swift.RandomAccessCollection, RowContent: View, Data.Element: Swift.Identifiable {
+        self.selection = selection
+        self.content = ForEach.init(data) { element in
+            HStack {
+                rowContent(element)
+            }
+        }
+    }
+    public init<Data, ID, RowContent>(_ data: Data, id: Swift.KeyPath<Data.Element, ID>, selection: Binding<SelectionValue?>?, @ViewBuilder rowContent: @escaping (Data.Element) -> RowContent) where Content == ForEach<Data, ID, HStack<RowContent>>, Data: Swift.RandomAccessCollection, ID: Swift.Hashable, RowContent: View {
+        self.selection = selection
+        self.content = ForEach.init(data, id: id) { element in
+            HStack {
+                rowContent(element)
+            }
+        }
+    }
+    public init<RowContent>(_ data: Swift.Range<Swift.Int>, selection: Binding<SelectionValue?>?, @ViewBuilder rowContent: @escaping (Swift.Int) -> RowContent) where Content == ForEach<Swift.Range<Swift.Int>, Swift.Int, HStack<RowContent>>, RowContent: View {
+        self.selection = selection
+        self.content = ForEach.init(data) { element in
+            HStack {
+                rowContent(element)
+            }
+        }
+    }
 }
 
 extension List: Primitive { }
