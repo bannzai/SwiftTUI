a9f878fd7c86b5841ce452aeeb5fefc3aa29d5ce
bannzai <kingkong999yhirose@gmail.com>
2020-04-18 01:29:07 +0900

    :sparkles: add failure test for ForEach

---
+++ Tests/SwiftTUITests/Primitive/ForEachTests.swift
@@ -0,0 +1,35 @@
+//
+//  ForEachTests.swift
+//  SwiftTUITests
+//
+//  Created by Yudai.Hirose on 2020/04/17.
+//
+
+import XCTest
+@testable import SwiftTUI
+
+class ForEachTests: XCTestCase {
+    override func setUp() {
+        super.setUp()
+        
+        mainScreen = DummyScreen.init()
+    }
+
+    func testChildrenPosition() throws {
+        XCTContext.runActivity(named: "when call border(.blue)") { (_) in
+            let view = ForEach((0..<2)) { (element: Int) in
+                Text("\(element)")
+            }
+            
+            let graph = prepareViewGraph(view: view)
+            let visitor = ViewSetRectVisitor()
+            graph.accept(visitor: visitor)
+            
+            XCTAssertEqual(graph.children.count, 2)
+            graph.children.forEach { child in
+                XCTAssertTrue(child.anyView is Text)
+                XCTAssertEqual(child.rect.origin, .zero)
+            }
+        }
+    }
+}
