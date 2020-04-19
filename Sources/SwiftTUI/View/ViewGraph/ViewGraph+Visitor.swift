4dc5386c614809e789014bc4030b465bb528228b
bannzai <kingkong999yhirose@gmail.com>
2020-04-19 09:23:17 +0900

    :recycle: add accept function

--- Sources/SwiftTUI/View/ViewGraph/ViewGraph+Visitor.swift
+++ Sources/SwiftTUI/View/ViewGraph/ViewGraph+Visitor.swift
@@ -8,7 +8,9 @@
 import Foundation
 
 extension ViewGraph: ViewSetRectVisitorAcceptable {
-    
+    func accept(visitor: ViewSetRectVisitor) {
+        acceptForSetDimensions(visitor: visitor)
+    }
 }
 
 
