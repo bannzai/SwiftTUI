b9c689c4da54ab84e7590e887ff9bb7c16e98712
bannzai <kingkong999yhirose@gmail.com>
2020-04-21 00:27:35 +0900

    :sparekls: add failure test case

--- Tests/SwiftTUITests/Primitive/ForEachTests.swift
+++ Tests/SwiftTUITests/Primitive/ForEachTests.swift
@@ -339,4 +339,40 @@ class ForEachTests: XCTestCase {
             XCTAssertEqual(content.filter { $0 == "X" }.count, 2)
         }
     }
+    
+    func testWithComplexModifiers() {
+        XCTContext.runActivity(named: "when ForEach<Text> And ForEach is contained ParentView. And ForEach has .border.padding.border") { (_) in
+            let view = VStack {
+                ForEach((0..<2).map(Model.init(id:))) { element in
+                    Text("\(element.id)")
+                }
+                .border(Color.blue)
+                .padding()
+                .border(Color.red)
+            }
+            
+            let graph = prepareSizedGraph(view: view)
+            let driver = Driver()
+            let visitor = ViewContentVisitor(driver: driver)
+            
+            graph.accept(visitor: visitor)
+            let content = driver.content()
+            
+            let subject: (String) -> Int = { (delimiter: String) in
+                content.filter { String($0) == delimiter }.count
+            }
+            
+            let cornerDelimiter = Edge.Set.leadingTop.defaultDelimiter
+            let elementCount = 2
+
+            XCTAssertTrue(driver.storedForegroundColors.contains(.blue))
+            XCTAssertTrue(driver.storedForegroundColors.contains(.red))
+            XCTAssertEqual(driver.storedForegroundColors.last, Style.Color.foreground.color)
+            XCTAssertEqual(subject(cornerDelimiter), (1 * 4 * elementCount) * 2)
+            XCTAssertEqual(subject(Edge.Set.vertical.defaultDelimiter), (2 * 2 * elementCount) + (3 * 2 * elementCount))
+            XCTAssertEqual(subject(Edge.Set.horizontal.defaultDelimiter), (2 * 2 * elementCount) + (3 * 2 * elementCount))
+            XCTAssertEqual(content.filter { $0 == "0" }.count, 1)
+            XCTAssertEqual(content.filter { $0 == "1" }.count, 1)
+        }
+    }
 }
