a55ed0aa021b52be177787ebea3c03e0766d7e65
bannzai <kingkong999yhirose@gmail.com>
2020-05-31 08:51:31 +0900

    :sparkles: Implement set position visitor

--- Sources/SwiftTUI/View/ViewGraph/ViewSetPositionVisitor.swift
+++ Sources/SwiftTUI/View/ViewGraph/ViewSetPositionVisitor.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+internal protocol ViewSetPositionVisitorAcceptable {
+    func accept(visitor: ViewSetPositionVisitor)
+}
+
+internal final class ViewSetPositionVisitor: Visitor {
+    internal init() { }
+    
+    internal var current: ViewGraph!
+    internal func visit<T: View>(_ content: T) {
+        debugLogger.debug(userInfo: "begin set positon visitor: \(type(of: content))")
+        defer {
+            debugLogger.debug(userInfo: "end set positon visitor: \(type(of: content))")
+        }
+        switch content {
+        case let acceptable as ViewSetPositionVisitorAcceptable:
+            return acceptable.accept(visitor: self)
+        case _:
+            return visit(content.body)
+        }
+    }
+}
