86123e3bd01ada634e031eb524cabd8fd4121484
bannzai <star.kojiki@gmail.com>
2025-07-03 18:14:45 +0900

    Fix Text

--- Sources/SwiftTUI/Text.swift
+++ Sources/SwiftTUI/Text.swift
@@ -1,11 +1,11 @@
-public struct Text {
+public struct Text: View {
   let content: String
 
   public init(_ content: String) {
     self.content = content
   }
 
-  public func render() {
-    print(content)
+  public func render(into buffer: inout [String]) {
+    buffer.append(content)
   }
 }
