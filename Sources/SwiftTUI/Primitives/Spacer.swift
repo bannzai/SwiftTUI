3cd6d8292bb2a5200ebc928a15a49886c310e3f2
bannzai <star.kojiki@gmail.com>
2025-07-04 06:14:45 +0900

    Add spacer

---
+++ Sources/SwiftTUI/Primitives/Spacer.swift
@@ -0,0 +1,22 @@
+import yoga
+
+/// 伸縮する空白
+public final class Spacer: LayoutView {
+
+  public init() {}
+
+  // 幅・高さ 0、flexGrow=1
+  public func makeNode() -> YogaNode {
+    let n = YogaNode()
+    n.setSize(width: 0, height: 0)
+    n.setFlexGrow(1)
+    return n
+  }
+
+  public func paint(origin: (x: Int, y: Int), into buf: inout [String]) {
+    // 描画するものは無し
+  }
+
+  // View プロトコル互換
+  public func render(into buffer: inout [String]) {}
+}
