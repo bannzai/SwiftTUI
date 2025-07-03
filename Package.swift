b2575c004daee93172c98a55668b0b5512d874b1
bannzai <star.kojiki@gmail.com>
2025-07-03 19:04:44 +0900

    Specify macOS platform

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
   name: "SwiftTUI",
+  platforms: [
+    .macOS(.v14),
+  ],
   products: [
     .library(name: "SwiftTUI", targets: ["SwiftTUI"]),
     .executable(name: "ExampleApp", targets: ["ExampleApp"]),
