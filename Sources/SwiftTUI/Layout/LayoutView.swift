28622860a45cf17aceb8fca0644fdf934f084e81
bannzai <star.kojiki@gmail.com>
2025-07-03 21:41:40 +0900

    Add blank implementation

--- Sources/SwiftTUI/Layout/LayoutView.swift
+++ Sources/SwiftTUI/Layout/LayoutView.swift
@@ -3,3 +3,10 @@ protocol LayoutView: View {
   func makeNode() -> YogaNode
   func paint(origin: (x: Int, y: Int), into buffer: inout [String])
 }
+
+extension LayoutView {
+  // View 要件を満たすだけ。RenderLoop では使わない
+  public func render(into buffer: inout [String]) {
+    // no-op で OK
+  }
+}
