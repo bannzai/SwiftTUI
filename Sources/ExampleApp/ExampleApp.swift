54cb145a9d78b64584435da09d24a6e63a1460da
bannzai <star.kojiki@gmail.com>
2025-07-03 20:19:22 +0900

    Fix

--- Sources/ExampleApp/ExampleApp.swift
+++ Sources/ExampleApp/ExampleApp.swift
@@ -1,22 +1,21 @@
-import SwiftTUI
 import Dispatch
+import SwiftTUI
 
 struct CounterView: View {
   @State private var count = 0
 
   func render(into buffer: inout [String]) {
     buffer.append("Count: \(count)")
-    buffer.append("Press 'i' to increment / ESC で終了")
+    buffer.append("Press 'i' to increment / ESC to quit")
   }
 
   func handle(event: KeyboardEvent) -> Bool {
     switch event.key {
     case .character("i"):
-      count += 1          // @State → scheduleRedraw()
+      count += 1
       return true
     case .escape:
-      // アプリ終了フラグを立てても良い
-      return false
+      exit(0)
     default:
       return false
     }
@@ -26,8 +25,8 @@ struct CounterView: View {
 @main
 struct ExampleApp {
   static func main() {
-    RenderLoop.mount { CounterView() }
-    // 以後は InputLoop が非同期で動くためメインスレッドをブロックしない
+    var counter = CounterView()
+    RenderLoop.mount { counter }
     Dispatch.dispatchMain()
   }
 }
