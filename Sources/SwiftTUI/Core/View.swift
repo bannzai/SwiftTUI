fc184532853ea7d333408d93aec2013e0b58e1a8
bannzai <star.kojiki@gmail.com>
2025-07-03 19:03:15 +0900

    Update Core/View interface

--- Sources/SwiftTUI/Core/View.swift
+++ Sources/SwiftTUI/Core/View.swift
@@ -1,3 +1,10 @@
 public protocol View {
   func render(into buffer: inout [String])
+
+  /// キーイベントを処理したら true を返す（デフォルトは false）
+  func handle(event: KeyboardEvent) -> Bool
+}
+
+public extension View {
+  func handle(event: KeyboardEvent) -> Bool { false }
 }
